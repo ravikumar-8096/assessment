03d709687e0fe08410725477ac2b866f12c9280f
ravikumar8096 <>
2023-09-09 09:36:39 +0530

    First commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe("<App />", () => {
+
+  test('render email input', () => {
+    render(<App />);
+
+    const inputEl = screen.getByTestId("numberToprint");
+    expect(inputEl).toBeInTheDocument();
+    expect(inputEl).toHaveAttribute("type", "number");
+
+    const width = screen.getByTestId("width");
+    expect(width).toBeInTheDocument();
+    expect(width).toHaveAttribute("type", "number");
+
+    const height = screen.getByTestId("height");
+    expect(height).toBeInTheDocument();
+    expect(height).toHaveAttribute("type", "number");
+  });
+
+  it('tests the onChange event for sequence of numbers', () => {
+    render(<App />)
+    const input = screen.getByLabelText('number-input')
+    let sampleNumbers = '1,2,3,4,5';
+    const value = 5;
+    fireEvent.change(input, {
+      target: {
+        value
+      }
+    })
+    expect(input).toHaveValue(5) // the state has been updated.
+    expect(screen.getAllByTestId("numbersVal")).toHaveLength(5)
+  })
+
+  it('tests the onChange event height input of Area', () => {
+    render(<App />)
+    const input = screen.getByLabelText('height-input')
+    const value =4;
+    fireEvent.change(input, {
+      target: {
+        value
+      }
+    })
+    expect(input).toHaveValue(4) // the state has been updated.
+  })
+
+  it('tests the onChange event width input of Area', () => {
+    render(<App />)
+    const input = screen.getByLabelText('width-input')
+    const value =4;
+    fireEvent.change(input, {
+      target: {
+        value
+      }
+    })
+    expect(input).toHaveValue(4) // the state has been updated.
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const [value, setValue] = useState<number | undefined | any>();
+  const [value2, setHeight] = useState<number>();
+  const [value3, setWidth] = useState<number>();
+
+  const [item1, setItem1] = useState<Array<number>>([]);
+  const [area, setarea] = useState<number>();
+
+
+  const handleOnchage = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  const handleOnchangeWidth = (e: any) => {
+    setHeight(e.target.value);
+  };
+
+  const handleOnchageHeight = (e: any) => {
+    setWidth(e.target.value);
+  };
+
+  let items: any;
+  let arr: Array<number> = [];
+
+  useEffect(() => {
+    let i = 0;
+    while (i < value) {
+      i++;
+      let val: any = i + ', '
+      arr.push(val);
+      setItem1(arr);
+    }
+
+    if (value2 && value3) {
+      setarea(value2 * value3);
+    }
+  }, [value, value3]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>Enter number to print sequence</h3>
+      <input type='number' data-testid="numberToprint" onChange={handleOnchage} value={value} aria-label="number-input" />
+      <br />
+      <span >{'Numbers :'}</span>
+      {value && item1.map((item: any) => (
+        <span data-testid="numbersVal">{item}</span>
+      ))}
+      <br />
+      <form>
+        <h3>Enter height and width</h3>
+        <span>{'Height :'}</span> <input type='number' data-testid="height" onChange={handleOnchangeWidth} aria-label="height-input"/>
+        <span>{'Width :'}</span> <input type='number' data-testid="width" onChange={handleOnchageHeight} aria-label="width-input"/>
+      </form>
+      <br />
+      <span>{'Area :'}</span>
+      <span data-testid="area">{area}</span>
     </div>
   );
 }
-
-export default App;
